679543e5c3588d381f6e3c903254962356f64476
Rodrigo Peleias <rodrigo.peleias@clickbus.com>
2019-09-12 14:38:44 -0300

    Integrated screen

--- src/main/java/com/clickbus/xpcourse/Main.java
+++ src/main/java/com/clickbus/xpcourse/Main.java
@@ -1,15 +1,15 @@
 package com.clickbus.xpcourse;
-
 import com.clickbus.xpcourse.jogos.Jogo;
 import com.clickbus.xpcourse.jogos.SpaceOfGilver;
-
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
-
 public class Main {
 
     public static void main(String[] args) throws InterruptedException, IOException {
         Jogo jogo = new SpaceOfGilver();
-
+        shipMovement(jogo);
         while (true) {
             System.out.println("----------------------------------------------");
             System.out.println(jogo.tela());
@@ -18,9 +18,24 @@ public class Main {
             Thread.sleep(3000);
         }
     }
-
     public static void clearScreen() throws IOException {
         System.out.println("\n\n\n\n\n");
-//        System.out.flush();
+    }
+    public static void shipMovement(final Jogo jogo) {
+        JFrame frame = new JFrame();
+        JButton button = new JButton();
+        frame.add(button);
+        frame.setVisible(true);
+        button.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    jogo.esquerda();
+                }
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    jogo.direita();
+                }
+            }
+        });
     }
 }
