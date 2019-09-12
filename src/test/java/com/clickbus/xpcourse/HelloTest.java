491bd586a49a10de6d2db2f03808c80a41bef3a5
Rodrigo Peleias <rodrigo.peleias@clickbus.com>
2019-09-12 09:32:37 -0300

    CReated project structure

--- src/test/java/com/clickbus/xpcourse/HelloTest.java
+++ src/test/java/com/clickbus/xpcourse/HelloTest.java
@@ -1,4 +1,20 @@
 package com.clickbus.xpcourse;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class HelloTest {
+
+    private Hello hello;
+
+    @Before
+    public void setUp() throws Exception {
+        hello = new Hello();
+    }
+
+    @Test
+    public void shoudSayHello() {
+        //Assert.assertEquals("Hel");
+    }
 }
