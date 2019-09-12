c71094cd35f1736a9d286f9e6ad7f21c26706cab
Rodrigo Peleias <rodrigo.peleias@clickbus.com>
2019-09-12 11:59:06 -0300

    Refactored screen implementation

--- src/main/java/com/clickbus/xpcourse/jogos/SpaceOfGilver.java
+++ src/main/java/com/clickbus/xpcourse/jogos/SpaceOfGilver.java
@@ -2,12 +2,28 @@ package com.clickbus.xpcourse.jogos;
 
 public class SpaceOfGilver implements Jogo {
 
-    char[][] gameLineArray = new char[5][5];
+    private static int LINE_SIZE = 5;
+    private static int COLUMN_SIZE = 5;
+
+    char[][] gameLineArray = new char[LINE_SIZE][COLUMN_SIZE];
+
+    public SpaceOfGilver() {
+        for (int i = 0; i < LINE_SIZE; i++) {
+            for (int j = 0; j < COLUMN_SIZE; j++) {
+                gameLineArray[i][j] = ' ';
+            }
+        }
+    }
 
     public String tela() {
-        String line = "     \n";
-        String gameLine = line + line + line + line + line;
-        return gameLine;
+        String tela = "";
+        for (int i = 0; i < LINE_SIZE; i++) {
+            for (int j = 0; j < COLUMN_SIZE; j++) {
+                tela += gameLineArray[i][j];
+            }
+            tela += "\n";
+        }
+        return tela;
     }
 
     public void direita() {
